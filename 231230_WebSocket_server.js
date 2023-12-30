cfcbba310d8d34dfc95ed41dee551b31a7522903
Manabio <155219634+Manabio@users.noreply.github.com>
2023-12-30 20:17:54 +0900

    Update 231230_WebSocket_server.js

--- 231230_WebSocket_server.js
+++ 231230_WebSocket_server.js
@@ -5,7 +5,7 @@ const http = require('http');
 const path = require('path');
 const socketIO = require('socket.io');
 const app = express();
-const server = http.Server(app);
+const server = "https://websocket-test-8ois.onrender.com";
 const io = socketIO(server);
 
 
